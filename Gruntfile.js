855a5e28c2b9c5428fbc6c8e9b71a3bda3d79cb7
Louis-Olivier Brassard <louis.oli.br@gmail.com>
2015-04-03 19:09:11 -0400

    fixed pkg reading in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
 	require('load-grunt-tasks');
 
 	grunt.initConfig({
-		pkg: require('package.json'),
+		pkg: grunt.file.readJSON('package.json'),
 
 		banner:
 		'/**' +
