9a62db4b9607e43fe2c1c32cdf4602d2510a1d4e
Louis-Olivier Brassard <louis.oli.br@gmail.com>
2015-04-03 20:25:19 -0400

    add src dir, commit build files in assets and dist

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+/** 
+ * @authors Matthew Hartman <hello@matthewhartman.com.au>,Louis-Olivier Brassard <louis.oli.br@gmail.com> 
+ * @version 1.10.0 
+ * @description Base is a super simple, responsive framework, built to work on all devices big, small and in-between. 
+ * @license MIT 
+ */ 
